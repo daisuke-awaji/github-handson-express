56fd432eff489aee2dc6740376e315e2134a7ff8
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2019-07-23 19:32:23 +0900

    ユーザがいない場合のテスト

--- src/index.js
+++ src/index.js
@@ -8,26 +8,49 @@ app.get('/', function (request, response) {
     response.send('Hello World!')
 });
 
-app.get('/user/:userId', function (request, response) {
-    userId = request.params
-    user = {
-        id: userId,
-        name: 'Smith'
+const users = [
+    {
+        id: 1,
+        name: 'Bob',
+        email: 'hi@azat.co'
+    },
+    {
+        id: 2,
+        name: 'Smith',
+        email: 'smith@mail.com'
+    },
+    {
+        id: 3,
+        name: 'Tom',
+        email: 'tomtom@gmail.com'
     }
-    response.send(user)
+];
+
+var findUserByUserId = function (userId, callback) {
+    for (let i in users) {
+        if (users[i].id.toString() === userId) {
+            return callback(null, users[i]);
+        };
+    };
+    return callback(new Error('No user matching ' + userId));
+};
+
+
+app.get('/user/:userId', function (request, response) {
+    findUserByUserId(request.params.userId, (error, user) => {
+        if (error == null) {
+            response.send(user)
+        } else {
+            errorMsg = {
+                message: error.stack
+            }
+            response.status(404)
+            response.send(errorMsg)
+        }
+    })
 });
 
 app.get('/users', function (request, response) {
-    users = [
-        {
-            name: 'Bob',
-            email: 'bob@email.com'
-        },
-        {
-            name: 'Smith',
-            email: 'smithemail.com'
-        }
-    ]
     response.send(users)
 });
 
