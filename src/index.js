45d62fb81ec695fee849ee3b6e6035bea0bc77fd
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2019-07-23 15:42:43 +0900

    nodemon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
     response.send('Hello World!')
+});
+
+app.get('/user/:userId', function (request, response) {
+    userId = request.params
+    user = {
+        id: userId,
+        name: 'Smith'
+    }
+    response.send(user)
 });
 
 app.get('/users', function (request, response) {
